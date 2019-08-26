3dd3a7dc855d03b915be181c5a16cfc8e6c2441c
max-509 <123456vershinin@gmail.com>
2019-08-26 06:44:39 -0400

    new vect

--- serial/blocks_external.cpp
+++ serial/blocks_external.cpp
@@ -19,6 +19,7 @@ inline __m256 vect_calc_radius(__m256 dx, __m256 dy, __m256 dz) {
 }
 
 int main(int argc, char const *argv[]) {
+    // omp_set_num_threads(1);
     std::ifstream data_file, receivers_file;
     data_file.open("../Data_noise_free.bin", std::ios::binary);
     if (!data_file.is_open()) {
@@ -34,17 +35,21 @@ int main(int argc, char const *argv[]) {
     size_t rec_count = 2000;
     size_t times = 10000;
 
-    std::unique_ptr<float[]> rec_times{new float[rec_count*times]()};
+    std::unique_ptr<float[], std::function<void(float[])>> rec_times{(float*)_mm_malloc(sizeof(float)*times*rec_count, 32), [](float f[]) { 
+        _mm_free(f); 
+    }};
     data_file.read(reinterpret_cast<char*>(rec_times.get()), rec_count*times*sizeof(float));
 
-    std::unique_ptr<float[]> rec_coords{new float[rec_count*3]()};
+    std::unique_ptr<float[], std::function<void(float[])>> rec_coords{(float*)_mm_malloc(sizeof(float)*3*rec_count, 32), [](float f[]) {
+        _mm_free(f);
+    }};
     receivers_file.read(reinterpret_cast<char*>(rec_coords.get()), rec_count*3*sizeof(float));
 
     float dt = 2e-3;
 
-    size_t nx = 50;
-    size_t ny = 50;
-    size_t nz = 50;
+    size_t nx = 10;
+    size_t ny = 10;
+    size_t nz = 10;
 
     float vv = 3000;
 
@@ -55,7 +60,9 @@ int main(int argc, char const *argv[]) {
     long long z0 = 500;
     long long z1 = 2500;
 
-    std::unique_ptr<float[]> area_discr{new float[times*nx*ny*nz]()};
+    std::unique_ptr<float[], std::function<void(float[])>> area_discr{(float*)_mm_malloc(sizeof(float)*times*nx*ny*nz, 32), [](float f[]) {
+        _mm_free(f);
+    }};
 
     float dx, dy, dz;
     if (1 < nx) dx = ((float)(x1-x0))/(nx-1);
@@ -73,16 +80,20 @@ int main(int argc, char const *argv[]) {
         times_block_size = atoi(argv[2]);
     }
 
-    std::unique_ptr<size_t[]> min_ind_arr{new size_t[nx*ny*nz]()};
-    std::unique_ptr<size_t[]> ind_arr{new size_t[nx*ny*nz*rec_count]()};
+    std::unique_ptr<int[], std::function<void(int[])>> min_ind_arr{(int*)_mm_malloc(sizeof(int)*nx*ny*nz, 32), [](int u[]) {
+        _mm_free(u);
+    }};
+    std::unique_ptr<int[], std::function<void(int[])>> ind_arr{(int*)_mm_malloc(sizeof(int)*nx*ny*nz, 64), [](int u[]) {
+        _mm_free(u);
+    }};
     __m256 vect_rec_coord[(rec_count/8)*3];
     t1 = omp_get_wtime();
     //algorithm
     //******************************************************//
-    #pragma omp parallel
+    //#pragma omp parallel
     {
         if (rec_count >= 8) {
-            #pragma omp for collapse(2)
+           //#pragma omp for collapse(2)
             for (size_t m = 0; m < rec_count; m+=8) {
                 for (size_t i = 0; i < 3; ++i) {
                     vect_rec_coord[(m/8)*3+i] = _mm256_set_ps(rec_coords[(m+7)*3+i], rec_coords[(m+6)*3+i], rec_coords[(m+5)*3+i], rec_coords[(m+4)*3+i],
@@ -90,7 +101,7 @@ int main(int argc, char const *argv[]) {
                 }
             }
             __m256 vect_ind_min;
-            #pragma omp for collapse(3)
+      //      #pragma omp for collapse(3)
             for (size_t i = 0; i < nz; ++i) {
                 for (size_t j = 0; j < nx; ++j) {
                     for (size_t k = 0; k < ny; ++k) {
@@ -106,11 +117,20 @@ int main(int argc, char const *argv[]) {
                             } else {
                                 vect_ind_min = vect_ind;
                             }
-                            float temp_ind[8];
-                            _mm256_store_ps(temp_ind, vect_ind);
-                            for (size_t v = 0; v < 8; ++v) {
-                                ind_arr[i*nx*ny*rec_count+j*ny*rec_count+k*rec_count+m+v] = temp_ind[v];
-                            }
+                            // float temp_ind[8];
+                            // _mm256_store_ps(temp_ind, vect_ind);
+                            // for (size_t v = 0; v < 8; ++v) {
+                            //     ind_arr[i*nx*ny*rec_count+j*ny*rec_count+k*rec_count+m+v] = temp_ind[v];
+                            // }
+                            int cast_ind_vect2[8] __attribute__ ((aligned (64)));
+                            //__m256i cast_ind_vect = _mm256_castps_si256(vect_ind);
+
+                            __m256i cast_ind_vect = _mm256_set_epi32(vect_ind[7],vect_ind[6],vect_ind[5],vect_ind[4],vect_ind[3],vect_ind[2],vect_ind[1],vect_ind[0]);
+                            _mm256_store_si256((__m256i*)(&ind_arr[i*nx*ny*rec_count+j*ny*rec_count+k*rec_count+m]), cast_ind_vect);
+                            // std::cout << cast_ind_vect2[0] << " " << cast_ind_vect2[1] << " " << cast_ind_vect2[2] << " " << cast_ind_vect2[3] << " " 
+                            // << cast_ind_vect2[4] << " " << cast_ind_vect2[5] << " " << cast_ind_vect2[6] << " " << cast_ind_vect2[7] << std::endl;
+                            //__m256i cast_ind_vect = _mm256_castps_si256(vect_ind);
+                            //_mm256_store_si256((__m256i*)(&ind_arr[i*nx*ny*rec_count+j*ny*rec_count+k*rec_count+m]), *(__m256i*)cast_ind_vect);
                         }
 
                         float min_indexes[8];
@@ -129,7 +149,7 @@ int main(int argc, char const *argv[]) {
                 }
             }
         } else {
-            #pragma omp for collapse(3)
+        //    #pragma omp for collapse(3)
             for (size_t i = 0; i < nz; ++i) {
                 for (size_t j = 0; j < nx; ++j) {
                     for (size_t k = 0; k < ny; ++k) {
@@ -152,23 +172,34 @@ int main(int argc, char const *argv[]) {
             }
         }
 
-        #pragma omp for 
+       // #pragma omp for 
     	for (size_t c_t = 0; c_t < times; c_t += times_block_size) {
             for (size_t c_r = 0; c_r < rec_count; c_r += rec_block_size) {
-    		  for (size_t i = 0; i < nz; ++i) {
+    		    for (size_t i = 0; i < nz; ++i) {
                     for (size_t j = 0; j < nx; ++j) {
                         for (size_t k = 0; k < ny; ++k) {
                             size_t min_ind = min_ind_arr[i*nx*ny+j*ny+k];
                             for (size_t m = c_r; m < std::min(c_r+rec_block_size, rec_count); ++m) {
                                 size_t ind = ind_arr[i*nx*ny*rec_count+j*ny*rec_count+k*rec_count+m]-min_ind;
                                 for (size_t l = c_t; l < std::min(c_t+times_block_size, times-ind); ++l) {
-                                    // for (size_t m = 0; m < rec_count; ++m) {
-                                    //  std::cout << rec_times[m*times+ind_arr[m]+l-min_ind] << std::endl;
-                                    // }
-                                    // return 0;
                                     area_discr[i*nx*ny*times+j*ny*times+k*times+l] += rec_times[m*times+ind+l];
                                     //реализовать ручную векторизацию, сравнить с автоматической
                                 }
+                                // size_t local_times_block_size = std::min(c_t+times_block_size, times-ind);
+                                // for (size_t l = c_t; l < local_times_block_size; l+=8) {
+                                //     if (l != local_times_block_size-(local_times_block_size%8)) {
+                                //         float* p_area_discr = area_discr.get()+i*nx*ny*times+j*ny*times+k*times+l;
+                                //         float* p_rec_times = rec_times.get()+m*times+ind+l;
+                                //         __m256 term1 = _mm256_load_ps(p_area_discr);
+                                //         __m256 term2 = _mm256_load_ps(p_rec_times);
+                                //         __m256 res = _mm256_add_ps(term1,  term2);
+                                //         _mm256_store_ps(p_area_discr, res);
+                                //     } else {
+                                //         for (; l < local_times_block_size; ++l) {
+                                //             area_discr[i*nx*ny*times+j*ny*times+k*times+l] += rec_times[m*times+ind+l];
+                                //         }
+                                //     }
+                                // }
                             }
                         }
                     }
@@ -182,7 +213,7 @@ int main(int argc, char const *argv[]) {
     t2 = omp_get_wtime();
 
     // std::ifstream results_file;
-    // results_file.open("../Summation_Results2.bin", std::ios::binary);
+    // results_file.open("./Results.bin", std::ios::binary);
     // if (!results_file.is_open()) {
     //     std::cerr << "Can't open Summation_Results.bin" << std::endl;
     //     return 1;
