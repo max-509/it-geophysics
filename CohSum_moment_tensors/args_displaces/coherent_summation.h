038d66fa49f2090a96ca2e09203d79c79939c6e1
max-509 <123456vershinin@gmail.com>
2019-11-28 07:10:54 -0500

    removing trash

--- CohSum_moment_tensors/args_displaces/coherent_summation.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COH_SUMM
-#define _COH_SUMM
-
-#include <cstdint>
-#include <cstddef>
-
-void compute(const float* rec_samples, const float* rec_coords, const float* displaces_vectors, const int32_t* sources_times, ptrdiff_t n_samples, ptrdiff_t n_rec, ptrdiff_t n_xyz, float* result_data);
-
-#endif /*_COH_SUMM*/
